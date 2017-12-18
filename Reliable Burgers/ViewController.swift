b7c712ddde14774c2acf982b66cc1e88ac53a6d9
Ariel Elkin <arielelkin@gmail.com>
2017-12-18 20:31:44 -0300

    remove didReceiveMemoryWarning boilerplate

--- Reliable Burgers/ViewController.swift
+++ Reliable Burgers/ViewController.swift
@@ -54,10 +54,4 @@ class ViewController: UIViewController {
             MSCrashes.generateTestCrash()
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
-
